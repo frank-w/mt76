67345ffc366a23e121afac75bf48b97113695eaa
Peter Chiu <chui-hao.chiu@mediatek.com>
2023-10-30 11:06:19 +0800

    mtk: wifi: mt76: add sanity check to prevent kernel crash
    wcid may not be initialized when mac80211 calls mt76.tx and it would lead to
    kernel crash.
    
    Signed-off-by: Peter Chiu <chui-hao.chiu@mediatek.com>

--- tx.c
+++ tx.c
@@ -345,6 +345,14 @@ mt76_tx(struct mt76_phy *phy, struct ieee80211_sta *sta,
 
 	info->hw_queue |= FIELD_PREP(MT_TX_HW_QUEUE_PHY, phy->band_idx);
 
+	if (!wcid->tx_pending.prev || !wcid->tx_pending.next) {
+		dev_warn(phy->dev->dev, "Un-initialized STA %pM wcid %d in mt76_tx\n",
+			 sta->addr, wcid->idx);
+
+		ieee80211_free_txskb(phy->hw, skb);
+		return;
+	}
+
 	spin_lock_bh(&wcid->tx_pending.lock);
 	__skb_queue_tail(&wcid->tx_pending, skb);
 	spin_unlock_bh(&wcid->tx_pending.lock);
